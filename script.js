0143acf7264b725d717923398a75c08cf6a43575
James Graca-Jones <jamesgracajones@gmail.com>
2022-10-03 16:29:25 +0100

    Write operate() function

--- script.js
+++ script.js
@@ -13,3 +13,7 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return multiply(num1, 1 / num2);
 }
+
+function operate(operation, num1, num2) {
+    return operation(num1, num2);
+};
