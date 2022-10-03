cf9ebe6dcbf66fdb22f1e572a65df7ad82253c83
James Graca-Jones <jamesgracajones@gmail.com>
2022-10-03 17:27:57 +0100

    Allow pressed digit to be displayed in #display div and store it too
    In script.js:
    - Add an event listener that, upon clicking a digit button:
        - Replaces the #display div's text content with the ID of the digit
        button (will be a string)
        - Stores an integer version of the ID in a variable 'displayValue'
        for use in next operation

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ function divide(num1, num2) {
 function operate(operation, num1, num2) {
     return operation(num1, num2);
 };
+
+// Create the functions that populate the display when you click the number buttons 
+// Should be storing the 'display value' in a variable somewhere for use in next 
+// operation
+// Use https://mrbuddh4.github.io/calculator/ as an example of the behaviour
+
+// 1. Create a variable called 'displayValue'
+// 2. Going to have to listen to see when a digit button is pressed
+// 3. When a new digit button is pressed, assign it to displayValue
+// 4. Put displayValue on the screen (or directly put the digit on the screen)
+
+const display = document.querySelector('#display')
+let displayValue;
+
+const digitButtons = document.querySelectorAll('.digit');
+
+digitButtons.forEach((button) => {
+
+    button.addEventListener('click', (e) => {
+        const digit = button['id'];
+        display.textContent = digit;
+
+        displayValue = +digit;
+    });
+});
