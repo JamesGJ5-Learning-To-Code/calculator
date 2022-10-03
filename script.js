6444db4ffdb8791ccc76ea45ae92a54cd5995435
James Graca-Jones <jamesgracajones@gmail.com>
2022-10-03 16:29:53 +0100

    Put semi-colon at end of each function declaration

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function add(num1, num2) {
     return num1 + num2;
-}
+};
 
 function subtract(num1, num2) {
     return add(num1, -num2);
-}
+};
 
 function multiply(num1, num2) {
     return num1 * num2;
-}
+};
 
 function divide(num1, num2) {
     return multiply(num1, 1 / num2);
-}
+};
 
 function operate(operation, num1, num2) {
     return operation(num1, num2);
