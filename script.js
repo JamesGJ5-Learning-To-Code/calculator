ecfc9b46773ed47356a5800bf8e74373a34b9ad4
James Graca-Jones <jamesgracajones@gmail.com>
2022-10-03 19:31:56 +0100

    Make comments about firstOperand and secondOperand more clear

--- script.js
+++ script.js
@@ -28,14 +28,6 @@ function operate(operation, num1, num2) {
 // 3. DONE When a new digit button is pressed, assign it to displayValue
 // 4. DONE Put displayValue on the screen (or directly put the digit on the screen)
 
-// Just need to store cumulativeResult and displayValue, then do operation on 
-// cumulativeResult and displayValue when operation is pressed--can't do it when 
-// number is pressed because you may want the value to have multiple digits
-
-// Will need to make sure the digits are appended to the display rather than 
-// replacing its content. Can do this by multiplying displayValue by 10 then 
-// adding digit to it, not so sure yet
-
 // So first, the calculator shows a 0. When you click a digit, it shows that digit. 
 // If you click another digit, it appends that digit to the current display value. 
 // You could append to 0 initially too, and then just get rid of the 0. However, 
@@ -45,9 +37,8 @@ function operate(operation, num1, num2) {
 // same display value is shown as when any other operator is pressed (the 
 // cumulative value so far)
 
-// So, have to store: accumulated value
-// Display value (reset this to 0 everytime an operator is pressed, but don't show it, 
-// unless pressing the clear button)
+// So, have to store: firstOperand
+// secondOperand
 
 let firstOperand = 0;
 
