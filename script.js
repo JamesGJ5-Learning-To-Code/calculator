dd80c6545e759c4109b4d02e4c0f19c6e6336796
James Graca-Jones <jamesgracajones@gmail.com>
2022-10-03 19:24:44 +0100

    Make it so that clicking a number digit simply appends it to the current

--- script.js
+++ script.js
@@ -28,6 +28,29 @@ function operate(operation, num1, num2) {
 // 3. DONE When a new digit button is pressed, assign it to displayValue
 // 4. DONE Put displayValue on the screen (or directly put the digit on the screen)
 
+// Just need to store cumulativeResult and displayValue, then do operation on 
+// cumulativeResult and displayValue when operation is pressed--can't do it when 
+// number is pressed because you may want the value to have multiple digits
+
+// Will need to make sure the digits are appended to the display rather than 
+// replacing its content. Can do this by multiplying displayValue by 10 then 
+// adding digit to it, not so sure yet
+
+// So first, the calculator shows a 0. When you click a digit, it shows that digit. 
+// If you click another digit, it appends that digit to the current display value. 
+// You could append to 0 initially too, and then just get rid of the 0. However, 
+// when an operator is pressed, the cumulative result so far is shown. Then, when 
+// a new digit is pressed, the operation is done on that cumulative result and 
+// the new digit, but the new digit alone is shown. When equals sign is pressed, 
+// same display value is shown as when any other operator is pressed (the 
+// cumulative value so far)
+
+// So, have to store: accumulated value
+// Display value (reset this to 0 everytime an operator is pressed, but don't show it, 
+// unless pressing the clear button)
+
+// let cumulativeResult = 0;
+
 const display = document.querySelector('#display')
 let displayValue = 0;
 
@@ -36,9 +59,10 @@ const digitButtons = document.querySelectorAll('.digit');
 digitButtons.forEach((button) => {
 
     button.addEventListener('click', (e) => {
-        const digit = button['id'];
-        display.textContent = digit;
+        const digit = +button['id'];
 
-        displayValue = +digit;
+        displayValue *= 10;
+        displayValue += digit;
+        // console.log(displayValue);
     });
 });
